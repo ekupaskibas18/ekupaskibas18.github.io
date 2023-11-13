2f783eb8ab222fd6b5d2b5e9eb22b0559b2fc700
ekupaskibas18 <rt@adtaco.com>
2023-11-13 17:49:43 +0800

    Added different images for homepage features

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -6,6 +6,7 @@ import styles from './styles.module.css';
 type FeatureItem = {
   title: string;
   link: string;
+  image: string;
   Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: JSX.Element;
 };
@@ -14,6 +15,7 @@ const FeatureList: FeatureItem[] = [
   {
     title: 'Sonnets',
     link: 'blogs/sonnets',
+    image: 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/pure_love_ay8a.svg',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
@@ -25,6 +27,7 @@ const FeatureList: FeatureItem[] = [
   {
     title: 'Love Letters',
     link: 'blogs/love-letters',
+    image: 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/heartbroken_cble.svg',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
@@ -36,6 +39,7 @@ const FeatureList: FeatureItem[] = [
   {
     title: 'Haikus',
     link: 'blogs/haikus',
+    image: 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/Bibliophile_re_xarc.svg',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
@@ -46,14 +50,19 @@ const FeatureList: FeatureItem[] = [
   },
 ];
 
-function Feature({title, link, Svg, description}: FeatureItem) {
+function Feature({title, link, image, Svg, description}: FeatureItem) {
   return (
     <Link to={link} className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+      <div className="text--center mb-6">
+        <img
+          src={image}
+          alt={title}
+          className="max-h-[10rem]"
+        />
+        {/* <Svg className={styles.featureSvg} role="img" /> */}
       </div>
       <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
+        <h3 className="font-semibold text-xl">{title}</h3>
         <p>{description}</p>
       </div>
     </Link>
