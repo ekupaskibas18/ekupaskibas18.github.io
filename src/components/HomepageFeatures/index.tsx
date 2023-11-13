404dd6f7714717b9436e20449d3351c7cb6dfaf3
ekupaskibas18 <rt@adtaco.com>
2023-11-13 23:41:33 +0800

    Added sub taglines

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -19,20 +19,7 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        {/* Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly. */}
-      </>
-    ),
-  },
-  {
-    title: 'Love Letters',
-    link: 'blogs/love-letters',
-    image: 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/heartbroken_cble.svg',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        {/* Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory. */}
+        Sonnets written from the heart. For the love of...
       </>
     ),
   },
@@ -43,8 +30,18 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        {/* Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer. */}
+        Sweetest thoughts in a few words.
+      </>
+    ),
+  },
+  {
+    title: 'Love Letters',
+    link: 'blogs/love-letters',
+    image: 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/heartbroken_cble.svg',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        Unsent love letters. For your eyes only.
       </>
     ),
   },
