2dbffc6276aca6fd7a4e56b0f87b34df29ed0182
randellyaptuazon <rt@adtaco.com>
2023-12-11 22:11:59 +0800

    Updated config, added haiku

---
+++ blogs/haikus/2023-12-11-dying-inside.md
@@ -0,0 +1,18 @@
+---
+slug: dying-inside
+title: Dying Inside
+authors: [randelltuazon]
+keywords: [randell tuazon, poetry, haiku]
+tags: [poetry, haiku]
+---
+
+<br/><br/><br/>
+
+I'm looking at you...  
+So happy, so full of life.
+***I'm dying inside.***  
+
+<footer>
+  — Randell Tuazon 
+  <div class="text-xs mt-2 text-stone-500">*Haiku: Dying Inside*</div>
+</footer>
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,15 +9,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://ekupaskibas18.github.io/',
+  url: 'https://randellyaptuazon.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ekupaskibas18', // Usually your GitHub org/user name.
-  projectName: 'ekupaskibas18.github.io', // Usually your repo name.
+  organizationName: 'randellyaptuazon', // Usually your GitHub org/user name.
+  projectName: 'randellyaptuazon.github.io', // Usually your repo name.
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
